e4f7cf887d02717665947f2a5026ad28c101cd8d
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2024-10-25 19:03:33 +0900

    [Silver II] Title: 최대 힙, Time: 808 ms, Memory: 32320 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+    
+        PriorityQueue<Integer> intCount = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int i = 0; i < N; i++) {
+            int n = Integer.parseInt(br.readLine());
+            if (n == 0 && intCount.size() == 0) {
+                System.out.println(0);
+            } 
+            else if (n == 0 && intCount.size() != 0) {
+                System.out.println(intCount.poll());
+            } 
+            else {
+                intCount.add(n);
+            }
+            
+        }
+    }
+}
