b5eef6442f01218a9a9c8096c3fbf99e2d65938f
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2024-10-27 13:59:41 +0900

    [Silver V] Title: LCM, Time: 104 ms, Memory: 14004 KB -BaekjoonHub

---
+++ 백준/Silver/5347. LCM/LCM.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int num1 = Integer.parseInt(st.nextToken());
+            int num2 = Integer.parseInt(st.nextToken());
+            int gcd = getGCD(num1, num2);
+            long lcm = (long) num1 * num2 / gcd;
+            System.out.println(lcm);
+        }
+    }
+
+    public static int getGCD(int num1, int num2) {
+        if (num1 % num2 == 0) {
+            return num2;
+        }
+        return getGCD(num2, num1%num2);
+    }
+}
