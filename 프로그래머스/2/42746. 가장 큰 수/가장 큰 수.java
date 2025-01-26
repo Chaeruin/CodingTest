29d54f6ff5b9901d6bc75582c5063cf69c7cd30d
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2025-01-26 14:47:21 +0900

    [level 2] Title: 가장 큰 수, Time: 2968.70 ms, Memory: 397 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -4,25 +4,24 @@ import java.io.*;
 class Solution {
     
     public String solution(int[] numbers) {
-        String[] arr = new String[numbers.length];
+        String answer = "";
 
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = String.valueOf(numbers[i]);
+        String[] strNumbers = new String[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            strNumbers[i] = Integer.toString(numbers[i]);
         }
 
-        Arrays.sort(arr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+        Arrays.sort(strNumbers, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
         
-        if (arr[0].equals("0")) {
+        if (strNumbers[0].equals("0")) {
            return "0";
         }
 
-        StringBuilder answer = new StringBuilder();
-
-        for (int i = 0; i < arr.length; i++) {
-            answer.append(arr[i]);
+        for (int i = 0; i < strNumbers.length; i++) {
+            answer += strNumbers[i];
         }
 
-
-        return answer.toString();
+        return answer;
     }
 }
