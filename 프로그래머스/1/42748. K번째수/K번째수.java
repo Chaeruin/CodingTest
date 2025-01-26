f5aea622153b486463fb3ddd482bdfda901a70bf
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2025-01-26 14:19:05 +0900

    [level 1] Title: K번째수, Time: 0.35 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        
+        int[] answer = new int[commands.length];
+        int idx = 0;
+        
+        for (int i = 0; i < commands.length; i++) {
+            int[] arr = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(arr);
+            answer[idx++] = arr[commands[i][2] - 1];
+        }
+        
+        return answer;
+    }
+}
