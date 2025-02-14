44bb47b78723a9cc85b5daf2f8ef1732657777de
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2025-02-14 17:38:04 +0900

    [Silver I] Title: 안전 영역, Time: 224 ms, Memory: 18676 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,53 +1,58 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
+class Main {
     static int N;
-    static int[][] arr;
+    static int[][] map;
+    static int count = 0;
     static boolean[][] visited;
-    static int max = 0, count;
-    static int answer = 1;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {1, -1, 0, 0};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         N = Integer.parseInt(br.readLine());
-        arr = new int[N][N];
-
+        
+        map = new int[N][N];
+        
         StringTokenizer st;
-
+        int max = 0;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-                max = Math.max(max, arr[i][j]);
+                map[i][j] = Integer.parseInt(st.nextToken());
+                max = Math.max(max, map[i][j]);
             }
         }
-
-        for (int h = 1; h <= max; h++) {
+        
+        int answer = 0;
+        for (int k = 0; k <= max; k++) {
             visited = new boolean[N][N];
             count = 0;
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
-                    if (arr[i][j] > h && !visited[i][j]) {
+                    if (map[i][j] > k && !visited[i][j]) {
+                        dfs(k, i, j);
                         count++;
-                        dfs(i, j, h);
                     }
                 }
             }
             answer = Math.max(answer, count);
         }
+        
         System.out.println(answer);
+        
     }
-    static void dfs(int x, int y, int h) {
+    
+    static void dfs(int pivot, int x, int y) {
         visited[x][y] = true;
+        
         for (int i = 0; i < 4; i++) {
-            int nx = dx[i] + x;
-            int ny = dy[i] + y;
-            if ((nx >= 0 && nx < N) && (ny >= 0 && ny < N) && !visited[nx][ny] && arr[nx][ny] > h) {
-                dfs(nx, ny, h);
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            
+            if (nx >= 0 && ny >= 0 && nx < N && ny < N && map[nx][ny] > pivot && !visited[nx][ny]) {
+                dfs(pivot, nx, ny);
             }
         }
     }
