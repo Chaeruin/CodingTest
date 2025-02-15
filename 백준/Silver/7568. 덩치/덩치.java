daf5710eba0081966ac303415e5adbfdfcd2ddb8
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2025-02-15 15:57:56 +0900

    [Silver V] Title: 덩치, Time: 100 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main (String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[N][2];
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+
+        // 입력 2차원 배열로 시키는 법!!! 익혀두기
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+            int rank = 1;
+            for (int j = 0; j < N; j++) {
+                if (i == j) continue;
+                if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) rank++;
+            }
+            sb.append(rank).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
