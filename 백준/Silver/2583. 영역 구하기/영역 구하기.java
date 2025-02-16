43297e3678107c3ce58725b5bc13ed20ff251b56
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2025-02-16 18:22:53 +0900

    [Silver I] Title: 영역 구하기, Time: 124 ms, Memory: 16164 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.java
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -1,75 +1,83 @@
+
 import java.util.*;
 import java.io.*;
 
-public class Main {
-    static boolean[][] linked;
-	static boolean[][] visited;
-	static int[] dx = {0,0,-1,1};
-	static int[] dy = {-1,1,0,0};
-	static List<Integer> result;
-	static int cnt, N, M, K;
+class Main {
+    static int M, N, K;
+    static int[][] map;
+    static boolean[][] visited;
+    static int count;
+    static List<Integer> result;
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {1, -1, 0, 0};
 
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        result = new LinkedList<>();
-        N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        linked = new boolean[M][N];
-        visited = new boolean[M][N];
-        cnt = 0;
+        map = new int[N][M];
+        visited = new boolean[N][M];
+        result = new ArrayList<>();
 
-        for (boolean a[]: linked) Arrays.fill(a, false);
+        for (int i = 0; i < N; i++) {
+            Arrays.fill(map[i], 0);
+        }
 
+        for (int i = 0; i < K; i++) {
+            st = new StringTokenizer(br.readLine());
+            int startX = Integer.parseInt(st.nextToken());
+            int startY = Integer.parseInt(st.nextToken());
+            int endX = Integer.parseInt(st.nextToken());
+            int endY = Integer.parseInt(st.nextToken());
 
-        for(int i = 0; i < K; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            int d = Integer.parseInt(st.nextToken());
-
-            for (int k = a; k < c; k++) {
-                for (int t = b; t < d; t++) {
-                    linked[k][t] = true;
+            for (int x = startX; x < endX; x++) {
+                for (int y = startY; y < endY; y++) {
+                    map[x][y] = 1;
                 }
             }
-		}
-		
-		
-		for(int x = 0; x < M; x++) {
-			for(int y = 0; y < N; y++) {
-				if(!linked[x][y] && !visited[x][y]) {
-                    cnt = 1;
-					dfs(x,y);
-					result.add(cnt);
-				}
-			}
-		}
+        }
 
-        Collections.sort(result);
-		
-		bw.write(result.size()+"\n");
-		for(int r : result) bw.write(r+" ");
-		bw.flush();
-		bw.close();
-    }
-
-    public static void dfs(int x, int y) {
-        visited[x][y] = true;
-
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (nx >= 0 && ny >= 0 && nx < M && ny < N && !linked[nx][ny] && !visited[nx][ny]) {
-                dfs(nx, ny);
-                cnt++;
+        count = 1;
+        for (int i = 0; i < N; i++) {
+            for (int j =0; j < M; j++) {
+                if (map[i][j] == 0 && !visited[i][j]) {
+                    bfs(i, j);
+                    result.add(count);
+                    count = 1;
+                }
             }
         }
-        
+
+        Collections.sort(result);
+
+        System.out.println(result.size());
+        for (int r: result) {
+            System.out.print(r + " ");
+        }
+
+    }
+
+    static void bfs(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[] {x, y});
+        visited[x][y] = true;
+
+        while(!queue.isEmpty()) {
+            int[] cur = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+
+                if (nx >= 0 && ny >= 0 && nx < N && ny < M && map[nx][ny] == 0 && !visited[nx][ny]) {
+                    queue.add(new int[] {nx, ny});
+                    count++;
+                    visited[nx][ny] = true;
+                }
+            }
+        }
     }
 }
