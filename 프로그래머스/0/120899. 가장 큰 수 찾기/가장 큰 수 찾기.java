ca032cdc4a63ed2c601cddc4a75579d5defb6770
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2025-06-03 16:09:30 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int[] array) {
+        int max = -1;
+        int index = -1;
+        
+        for (int i = 0; i < array.length; i++) {
+            if (max < array[i]) {
+                max = array[i];
+                index = i;
+            }
+        }
+        
+        return new int[] {max, index};
+    }
+}
