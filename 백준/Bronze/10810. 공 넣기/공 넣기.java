49497f638f4f10a36dc25cbcac3ba3f153cfbec3
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-01-11 13:34:14 +0900

    [Bronze III] Title: 공 넣기, Time: 120 ms, Memory: 15892 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -0,0 +1,36 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        boolean[] bool = new boolean[n];
+        for (int l = 0; l < m; l++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+            for (int h = (i - 1); h < j; h++) {
+                if (!bool[h]) {
+                    arr[h] = k;
+                    bool[h] = true;
+                }
+                else {
+                    arr[h] = k;
+                    bool[h] = false;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
