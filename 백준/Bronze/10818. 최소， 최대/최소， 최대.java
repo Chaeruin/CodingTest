b08c49504a7a1d5d7e59bf28c0fda1cc3b604663
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-01-11 12:55:35 +0900

    [Bronze III] Title: 최소, 최대, Time: 1124 ms, Memory: 125172 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,22 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    // 배열
+    // 리스트
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n];
+        List<Integer> list = new ArrayList<>();
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            list.add(Integer.parseInt(st.nextToken()));
         }
 
-        Arrays.sort(arr);
-        System.out.println(arr[0] + " " + arr[n - 1]);
+        list.sort(Comparator.naturalOrder());
+        System.out.println(list.get(0) + " " + list.get(list.size() - 1));
+
     }
 }
