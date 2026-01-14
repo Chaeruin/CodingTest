269843a509a557f330072dba9bbaf20109d26971
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-01-14 16:16:45 +0900

    [Bronze II] Title: 소수, Time: 112 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.java
+++ 백준/Bronze/2581. 소수/소수.java
@@ -1,53 +1,43 @@
-import java.util.Scanner;
- 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
- 
-	public static boolean prime[];
-	
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
-		
-		int M = in.nextInt();
-		int N = in.nextInt();
-		
-		prime = new boolean[N + 1];	// 배열 생성 
-		get_prime();
-		
-		
-		// 소수 합 및 최솟값 
-		int sum = 0;
-		int min = Integer.MAX_VALUE;
-		for(int i = M; i <= N; i++) {
-			if(prime[i] == false) {	// false = 소수 
-				sum += i;
-				if(min == Integer.MAX_VALUE) {	// 첫 소수가 최솟값임  
-					min = i;
-				}
-			}
-		}
-		
-		if(sum == 0) {	// 소수가 없다면 
-			System.out.println(-1);
-		}
-		else {
-			System.out.println(sum);
-			System.out.println(min);
-		}
-		
-	}
- 
-	
-	// 에라토스테네스 체 알고리즘 
-	public static void get_prime() {
-		prime[0] = true;
-		prime[1] = true;
-		
-		for(int i = 2; i <= Math.sqrt(prime.length); i++) {
-			for(int j = i * i; j < prime.length; j += i) {
-				prime[j] = true;
-			}
-		}
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int m = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        boolean hasPrime = false;
+
+        for (int i = m; i <= n; i++) {
+            if (is_Prime(i)) {
+                sum += i;
+                min = Math.min(min, i);
+                hasPrime = true;
+            }
+        }
+        if (hasPrime) {
+            System.out.println(sum);
+            System.out.println(min);
+        } else {
+            System.out.println("-1");
+        }
+    }
+
+    public static boolean is_Prime(int Number) {
+
+        if(Number == 1){
+            return false;
+        }
+
+        for(int i = 2; i <= Math.sqrt(Number); i++) {
+            if(Number % i == 0) return false;
+        }
+
+        return true;
+    }
 }
