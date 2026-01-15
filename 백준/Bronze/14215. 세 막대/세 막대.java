63ea93e1268443cc54061cbc9cf8ad1bb7be5f5b
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-01-15 18:56:32 +0900

    [Bronze III] Title: 세 막대, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/14215. 세 막대/세 막대.java
@@ -0,0 +1,40 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        
+        int tmp = 0;
+
+        if (a < b) {
+            tmp = a;
+            a = b;
+            b = tmp;
+        }
+        if (a < c) {
+            tmp = a;
+            a = c;
+            c = tmp;
+        }
+
+        while (true) {
+            int sum = a + b + c;
+            if (b + c > a) {
+                System.out.println(sum);
+                break;
+            } else {
+                a--;
+            }
+        }
+    }
+}
