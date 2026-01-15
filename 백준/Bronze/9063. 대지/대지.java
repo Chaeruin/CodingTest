4879279f46048b6c0108cc58bc1270d8b6e00df6
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-01-15 18:49:52 +0900

    [Bronze III] Title: 대지, Time: 264 ms, Memory: 27804 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.java
@@ -0,0 +1,40 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        if (n == 1) {
+            System.out.println("0");
+            return;
+        }
+
+        int minX = Integer.MAX_VALUE;
+        int minY = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE;
+        int maxY = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            minX = Math.min(minX, x);
+            minY = Math.min(minY, y);
+            maxX = Math.max(maxX, x);
+            maxY = Math.max(maxY, y);
+        }
+
+        int width = maxX - minX;
+        int height = maxY - minY;
+
+        System.out.println(width * height);
+    }
+}
