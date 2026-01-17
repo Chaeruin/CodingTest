91ba8513602cbe8f7089defa395327ecf300c007
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-01-17 15:12:09 +0900

    [Silver V] Title: 영화감독 숌, Time: 248 ms, Memory: 84904 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -1,20 +1,23 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int input = Integer.parseInt(br.readLine());
 
-        int number = 666;
-        int count = 1;
+        int count = 0;
 
-        while (count != N) {
-            number++;
-            if (String.valueOf(number).contains("666")) {
-                count++;
+        int devil = 665;
+
+        while (true) {
+            devil++;
+            if (Integer.toString(devil).contains("666")) count++;
+            if (count == input) {
+                System.out.println(devil);
+                break;
             }
         }
-        System.out.println(number);
     }
 }
