a9f3f1928cc90bad0674947493b12b859ac7329c
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-01-17 15:18:04 +0900

    [Silver IV] Title: 설탕 배달, Time: 100 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,22 +1,29 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int input = Integer.parseInt(br.readLine());
 
-        int N = sc.nextInt();
+        int count = 0;
 
-        int[] dp = new int[N+5];
-        int maxVal = 5000/3+1;
-
-        Arrays.fill(dp,maxVal);
-        dp[3] = dp[5] = 1;
-
-        for(int i = 6; i <= N+1; i++) {
-            dp[i] = Math.min(dp[i-3], dp[i-5]) + 1;
+        while (input > 0) {
+            if (input % 5 == 0) {
+                count += input / 5;
+                System.out.println(count);
+                return;
+            }
+            if (input < 3) {
+                System.out.println("-1");
+                return;
+            }
+            
+            input -= 3;
+            count++;
         }
 
-        System.out.println(String.valueOf(dp[N] >= maxVal ? -1 : dp[N]));
+        System.out.println(count);
     }
 }
