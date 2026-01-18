f9c58ee820df6366d515702a64d2b31f70e41d98
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-01-18 14:08:19 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1216 ms, Memory: 66280 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,34 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        int[][] list = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            String[] input = br.readLine().split(" ");
+            list[i][0] = Integer.parseInt(input[0]);
+            list[i][1] = Integer.parseInt(input[1]);
+        }
+
+        Arrays.sort(list, (x, y) -> {
+            if (x[0] == y[0]) {
+                return x[1] - y[1];
+            }
+            return x[0] - y[0];
+        });
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(list[i][0] + " " + list[i][1] );
+        }
+    }
+}
