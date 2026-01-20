424731982a262d74b2df9c1512fff3f1ef11cfd8
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-01-20 15:59:20 +0900

    [Silver III] Title: 분수 합, Time: 124 ms, Memory: 16040 KB -BaekjoonHub

--- 백준/Silver/1735. 분수 합/분수 합.java
+++ 백준/Silver/1735. 분수 합/분수 합.java
@@ -1,54 +1,37 @@
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int numerator1 = Integer.parseInt(st.nextToken());
-        int denominator1 = Integer.parseInt(st.nextToken());
+
+        int numeratorA = Integer.parseInt(st.nextToken());
+        int denominatorA = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
-        int numerator2 = Integer.parseInt(st.nextToken());
-        int denominator2 = Integer.parseInt(st.nextToken());
 
-        // new 분모 (통분 분모)
-        int lcmNum = lcm(denominator1, denominator2);
+        int numeratorB = Integer.parseInt(st.nextToken());
+        int denominatorB = Integer.parseInt(st.nextToken());
 
-        int div1 = divLcm(denominator1, lcmNum);
-        int div2 = divLcm(denominator2, lcmNum);
+        int newNum = (numeratorA * denominatorB) + (numeratorB * denominatorA);
+        int newDen = denominatorA * denominatorB;
 
-        // 통분해서 더한 분자
-        int newNumerator = numerator1 * div1 + numerator2 * div2;
+        int gcd = findGCD(newNum, newDen);
 
-        // 통분해서도 기약분수로 바꿔야함
-        if (gcd(lcmNum, newNumerator) == 1) {
-            System.out.println(newNumerator + " " + lcmNum);
-        } else {
-            int divGCDNum = gcd(lcmNum, newNumerator);
-            System.out.println(newNumerator/divGCDNum + " " + lcmNum/divGCDNum);
-        }
+        System.out.println((newNum / gcd) + " " + (newDen / gcd));
+
     }
 
-    static int gcd(int a, int b) {
+    static int findGCD(int a, int b) {
         while (b != 0) {
-            int temp = a % b;
+            int r = a % b;
             a = b;
-            b = temp;
+            b = r;
         }
         return a;
     }
-
-    static int lcm(int a, int b) {
-        int gcdNum = gcd(a, b);
-        return a * b / gcdNum;
-    }
-
-    static int divLcm(int a, int lcmNum) {
-        return lcmNum / a;
-    }
-
-
 }
