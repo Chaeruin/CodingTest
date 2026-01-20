2865c44c0879b8fbdc465213d0a7d6b73ddcec04
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-01-20 16:47:50 +0900

    [Silver III] Title: 소수 구하기, Time: 228 ms, Memory: 20540 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,30 +1,37 @@
-import java.io.*;
-import java.util.*;
 
-class Main {
-    static int M, N;
-    static int[] dp;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
-        
-        for (int i = M; i <= N; i++) {
-            if (isDecimal(i)) {
-                System.out.println(i);
+
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+
+        boolean[] prime = new boolean[n + 1]; // 아리스토테네스의 체
+
+        prime[0] = true;
+        prime[1] = true;
+
+        // 아리스토테네스의 체 초기화
+        for(int i = 2; i * i < prime.length; i++) {
+            if(prime[i]) continue;
+            for(int j = i * i; j < prime.length; j += i) {
+                prime[j] = true;
             }
         }
-    }
-    
-    static boolean isDecimal(int d) {
-        if (d == 1) return false;
-        for (int i = 2; i * i <= d; i++) {
-            if (d % i == 0) {
-                return false;
+
+        for(int i = m; i <= n; i++) {
+            if(!prime[i]) {
+                sb.append(i).append("\n");
             }
         }
-        return true;
+        System.out.println(sb);
     }
 }
