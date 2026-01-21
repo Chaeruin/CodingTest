1066cd2be64cb3a78c0f6e91e78a6c81c4f2035c
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-01-21 14:34:13 +0900

    [Silver IV] Title: 제로, Time: 216 ms, Memory: 23996 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,31 +1,27 @@
-import java.util.*;
-import java.io.*;
 
-class Main {
-    static int N;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int K = Integer.parseInt(br.readLine());
         Stack<Integer> stack = new Stack<>();
-        
-        N = Integer.parseInt(br.readLine());
-        
-        for (int i = 0; i < N; i++) {
+
+        for (int i = 0; i < K; i++) {
             int input = Integer.parseInt(br.readLine());
-            
-            if (input == 0) {
-                stack.pop();
-            } else {
-                stack.push(input);
-            }
+            if (input == 0) stack.pop();
+            else stack.push(input);
         }
-        
+
         int sum = 0;
+
         while (!stack.isEmpty()) {
             sum += stack.pop();
         }
-        
+
         System.out.println(sum);
     }
 }
-    
