c6fbbd30855f7360280da60ed0bb6b814243cbf4
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-01-21 12:56:39 +0900

    [Silver II] Title: 베르트랑 공준, Time: 464 ms, Memory: 14640 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -1,45 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-import java.io.*;
-import java.util.*;
-
-class Main {
-    static int[] arr1;
+public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());
-
-
-        while (N != 0) {
-            arr1 = new int[2 * N + 1];
-            eratosthenesSieve(N);
-
-            int count = 0;
-            for (int i = N + 1; i <= 2 * N; i++) {
-                if (arr1[i] != 0) count++;
-            }
-
-            System.out.println(count);
-
-            N = Integer.parseInt(br.readLine());
+        int input = 0;
+        while ((input = Integer.parseInt(br.readLine())) != 0 ) {
+            System.out.println(countPrime(input));
         }
     }
 
-    static void eratosthenesSieve(int N) {
-        for (int i = 2; i <= 2 * N; i++) {
-            arr1[i] = i;
+    static int countPrime(int n) {
+        int count = 0;
+        for (int i = n + 1; i <= 2 * n; i++) {
+            if (isPrime(i)) count++;
         }
-
-        for (int i = 2; i <= 2 * N; i++) {
-            if (arr1[i] == 0) {
-                continue;
-            }
-            for (int j = i + i; j <= 2 * N; j += i) {
-                arr1[j] = 0;
-            }
-        }
+        return count;
     }
 
-
+    static boolean isPrime(int n) {
+        if (n == 1) return false;
+        for (int j = 2; j <= Math.sqrt(n); j++) {
+            if (n % j == 0) return false;
+        }
+        return true;
+    }
 }
