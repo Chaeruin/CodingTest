5d078f6036d411732def78f23c0e2f5b9dfcf607
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-01-25 14:22:00 +0900

    [Silver II] Title: 통계학, Time: 484 ms, Memory: 49620 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.java
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -1,64 +1,52 @@
-import java.util.*;
-import java.io.*;
 
-class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
-    static int N;
-    static int[] arr;
-
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N];
 
+        int N = Integer.parseInt(br.readLine());
+
+        int[] list = new int[N];
+        double sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            list[i] = Integer.parseInt(br.readLine());
+            sum += list[i];
+        }
+
+        Arrays.sort(list);
+
+        int count = 0;
         int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-            max = Math.max(max, arr[i]);
-            min = Math.min(min, arr[i]);
+
+        int mod = list[0];
+        boolean check = false;
+
+        for(int i = 0; i < N - 1; i++) {
+            if (list[i] == list[i + 1]) {
+                count++;
+            } else {
+                count = 0;
+            }
+
+            if (max < count) {
+                max = count;
+                mod = list[i];
+                check = true;
+            } else if(max == count && check) {
+                mod = list[i];
+                check = false;
+            }
         }
 
-        Arrays.sort(arr);
+        System.out.println(Math.round(sum / N));
+        System.out.println(list[(N - 1) / 2]);
+        System.out.println(mod);
+        System.out.println(list[N - 1] - list[0]);
 
-        long sum = 0;
-        for (int i = 0; i < N; i++) {
-            sum += arr[i];
-        }
-
-        int avg = (int) Math.round((double) sum / N);
-        int middle = arr[N / 2];
-        int range = max - min;
-
-
-        System.out.println(avg);
-        System.out.println(middle);
-        System.out.println(findMostbind());
-        System.out.println(range);
-    }
-
-    static int findMostbind() {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < N; i++) {
-            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
-        }
-
-        List<Integer> keySet = new ArrayList<>(map.keySet());
-
-        keySet.sort((o1, o2) -> {
-                        int freqCompare = map.get(o2).compareTo(map.get(o1));
-                        return freqCompare == 0 ? Integer.compare(o1, o2) : freqCompare;
-                    });
-        
-        int answer = -1;
-        if (N == 1) {
-            answer = keySet.get(0);
-        } else if (map.get(keySet.get(0)) == map.get(keySet.get(1))) {
-            answer = keySet.get(0) < keySet.get(1) ? keySet.get(1) : keySet.get(0);
-        } else {
-            answer = keySet.get(0);
-        }
-
-        return answer;
     }
 }
