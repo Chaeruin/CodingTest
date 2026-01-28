80192059eeada259f26bd3482f98d263b7f5d968
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-01-28 11:12:12 +0900

    [Silver III] Title: N과 M (2), Time: 112 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,45 +1,40 @@
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-class Main {
-    static int M, N;
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static int N, M;
+    static boolean[] visit;
     static int[] arr;
-    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        arr = new int[N];
-        visited = new boolean[N];
 
-        for (int i = 1; i <= N; i++) {
-            arr[i - 1] = i;
-        }
-
-        combination(0, M);
+        visit = new boolean[N];
+        arr = new int[M];
+        dfs(1, 0);
+        System.out.println(sb);
     }
 
-    static void combination(int start, int r) {
-        if (r == 0) {
-            print();
-            return;
-        } else {
-            for (int i = start; i < N; i++) {
-                visited[i] = true;
-                combination(i + 1, r - 1);
-                visited[i] = false;
+    private static void dfs(int at, int depth) {
+        if (depth == M) {
+            for (int val : arr) {
+                sb.append(val).append(" ");
             }
+            sb.append("\n");
+            return;
         }
-    }
 
-    static void print() {
-        for (int i = 0; i < N; i++) {
-            if (visited[i] == true)
-                System.out.print(arr[i] + " ");
+        for (int i = at; i <= N; i++) {
+            arr[depth] = i;
+            dfs(i + 1, depth + 1);
         }
-        System.out.println();
     }
 }
