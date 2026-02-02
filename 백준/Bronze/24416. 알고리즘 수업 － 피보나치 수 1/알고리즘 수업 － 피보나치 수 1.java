9d874ce4d3aceb36dd1bc76aa3670450bc4f3193
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-02-02 12:56:59 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 492 ms, Memory: 16052 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
@@ -1,34 +1,43 @@
-import java.util.*;
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    static int cnt1, cnt2;
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+    static int countRecursion = 0;
+    static int countDp = 0;
+    static int[] fib;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        fib(n);
-        fibonacci(n);
+        fib = new int[n + 1];
 
-        System.out.println(cnt1 + " " + cnt2);
+        fibRecursion(n);
+
+        fibDp(n);
+
+        System.out.println(countRecursion + " " + countDp);
     }
 
-    public static int fib(int n) {
+    static int fibRecursion(int n) {
         if (n == 1 || n == 2) {
-            cnt1++;
+            countRecursion++;
             return 1;
         }
-        else return (fib(n-1) + fib(n-2));
+        else {
+            return fibRecursion(n - 1) + fibRecursion(n - 2);
+        }
     }
 
-    public static int fibonacci(int n) {
-        int[] f = new int[n+1];
-        if (n == 1 || n == 2) f[n] = 1;
+    static int fibDp(int n) {
+        fib[1] = fib[2] = 1;
         for (int i = 3; i <= n; i++) {
-            cnt2++;
-            f[i] = f[i -1] + f[i-2];
+            countDp++;
+            fib[i] = fib[i - 1] + fib[i - 2];
         }
-        return f[n];
+        return fib[n];
     }
 }
