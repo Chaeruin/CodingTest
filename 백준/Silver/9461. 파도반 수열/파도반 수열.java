ffdbafdea78d95cc04450f520d9d9d09ab576d6a
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-02-02 14:09:14 +0900

    [Silver III] Title: 파도반 수열, Time: 104 ms, Memory: 14028 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -1,35 +1,48 @@
+
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
- 
+import java.io.InputStreamReader;
+
 public class Main {
- 
-	static long[] seq = new long[101];
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		padovan();
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for (int i = 0; i < T; i++) {
-			sb.append(seq[Integer.parseInt(br.readLine())]).append('\n');
-		}
-		
-		System.out.println(sb);
-	}
- 
-	public static void padovan() {
-		
-		seq[1] = 1;
-		seq[2] = 1;
-		seq[3] = 1;
- 
-		for (int i = 4; i < 101; i++) {
-			seq[i] = seq[i - 2] + seq[i - 3];
-		}
-	}
- 
+
+    static long[] dp = new long[101];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        // 1 : 1
+        // 2 : 1
+        // 3 : 1
+        // 4 : 2
+        // 5 : 2
+        // 6 : 3
+        // 7 : 4
+        // 8 : 5
+        // 9 : 7
+        // 10 : 9
+        // 11 : 12
+        // 12 : 16
+        // 점화식 : dp[n] = dp[n - 1] + dp[n - 5]
+
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+        dp[4] = 2;
+        dp[5] = 2;
+        dp[6] = 3;
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int t = 0; t < T; t++) {
+            int N = Integer.parseInt(br.readLine());
+            if (N <= 6) sb.append(dp[N]).append("\n");
+            else {
+                for (int i = 6; i <= N; i++) {
+                    dp[i] = dp[i - 1] + dp[i - 5];
+                }
+                sb.append(dp[N]).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
 }
