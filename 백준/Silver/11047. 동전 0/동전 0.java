4f940be87aabf7477a6cac652239bfdf13f643dd
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-02-22 12:58:37 +0900

    [Silver IV] Title: 동전 0, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,32 +1,35 @@
-import java.util.*;
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(input[0]);
-        int K = Integer.parseInt(input[1]);
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        int[] coin = new int[N];
+        Integer[] coins = new Integer[N];
+        for (int i = 0; i < N; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(coins, Collections.reverseOrder());
+
+        int sum = 0;
 
         for (int i = 0; i < N; i++) {
-            coin[i] = Integer.parseInt(br.readLine());
-        }
-
-        int n = N - 1;
-        int count = 0;
-
-        while (K != 0 && n >= 0) {
-            if (K - coin[n] >= 0) {
-                K -= coin[n];
-                count++;
-            } else {
-                n--;
+            int div = K / coins[i];
+            if (div > 0) {
+                K -= div * coins[i];
+                sum += div;
             }
         }
-
-        System.out.println(count);
+        System.out.println(sum);
     }
 }
