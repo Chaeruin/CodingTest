b3906822319bf31838e2123f154fd5ee4a738631
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-03-15 12:15:05 +0900

    [Silver IV] Title: 수 찾기, Time: 552 ms, Memory: 51004 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,37 +1,39 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    static int N, M;
-    static int[] A, B;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        N = Integer.parseInt(br.readLine());
-        A = new int[N];
-        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        Set<Integer> set = new HashSet<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
+            set.add(Integer.parseInt(st.nextToken()));
         }
 
-        M = Integer.parseInt(br.readLine());
-        B = new int[M];
+        int M = Integer.parseInt(br.readLine());
+        List<Integer> list = new ArrayList<>();
+
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
-            B[i] = Integer.parseInt(st.nextToken());
+            list.add(Integer.parseInt(st.nextToken()));
         }
 
-        Arrays.parallelSort(A);
+        StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < M; i++) {
-            if (Arrays.binarySearch(A, B[i]) >= 0) {
-                System.out.println(1);
-            } else {
-                System.out.println(0);
-            }
+        for (int l : list) {
+            if (set.contains(l)) sb.append("1").append("\n");
+            else sb.append("0").append("\n");
         }
+
+        System.out.print(sb);
     }
 }
