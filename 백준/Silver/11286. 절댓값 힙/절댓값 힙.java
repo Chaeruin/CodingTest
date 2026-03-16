7fc123e867f383324d5fc318c6613908c937bf4f
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-03-16 15:41:00 +0900

    [Silver I] Title: 절댓값 힙, Time: 460 ms, Memory: 26020 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,64 +1,27 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static class absoluteInt {
-        int realNum; // 실제 값
-        int abNum;   // 절댓값
-
-        public absoluteInt(int realNum, int abNum) {
-            this.realNum = realNum;
-            this.abNum = abNum;
-        }
-    }
-
-    static class absoluteComparator implements Comparator<absoluteInt> {
-
-        @Override
-        public int compare(absoluteInt o1, absoluteInt o2) {
-            // o1 : -2 / o2 : -1   // o1 : 2 / o2 : 1
-            if (o1.abNum > o2.abNum) {
-                return 1;
-            } 
-            // o1 : -1 / o2 : 2  
-            else if (o1.abNum < o2.abNum && o1.realNum < o2.realNum) {
-                return -1;
-            }
-            // o1 : 1 / o2 : -2
-            else if (o1.abNum < o2.abNum && o1.realNum > o2.realNum) {
-                return -1;
-            }
-            // o1 : -1 / o2 : 1
-            else if (o1.abNum == o2.abNum && o1.realNum < o2.realNum){
-                return -1;
-            }
-            // o1 : 1 / o2 : -1
-            else if (o1.abNum == o2.abNum && o1.realNum > o2.realNum){
-                return 1;
-            }
-            return 0;
-        }
-    }
-
-    public static void main (String[] args) throws NumberFormatException, IOException {
-        PriorityQueue<absoluteInt> abQueue = new PriorityQueue<>(2, new absoluteComparator());
-
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> heap = new PriorityQueue<>(new Comparator<>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if (Math.abs(o1) != Math.abs(o2)) return Math.abs(o1) - Math.abs(o2);
+                return o1 - o2;
+            }
+        });
 
         for (int i = 0; i < N; i++) {
             int input = Integer.parseInt(br.readLine());
             if (input == 0) {
-                if (abQueue.size() == 0) {
-                    System.out.println(0);
-                }
-                else {
-                    System.out.println(abQueue.poll().realNum);
-                }
+                if (heap.isEmpty()) System.out.println(0);
+                else System.out.println(heap.poll());
             }
-            else {
-                abQueue.add(new absoluteInt(input, Math.abs(input)));
-            }
+            else heap.offer(input);
         }
+
     }
 }
