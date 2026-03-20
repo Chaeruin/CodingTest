11d119d8ef194bc392bc2de104ea87ece4acc59d
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-03-20 10:50:38 +0900

    [Gold V] Title: 암호 만들기, Time: 132 ms, Memory: 15044 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,12 +1,11 @@
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
-
     static int L, C;
-    static String[] arr;
-    static boolean[] visited;
+    static char[] code;
+    static char[] arr;
+    static List<Character> vlist;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,44 +13,49 @@ public class Main {
 
         L = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
+        code = new char[L];
+        arr = new char[C];
 
-        arr = new String[C];
-        visited = new boolean[C];
+        vlist = List.of('a', 'e', 'i', 'o', 'u');
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < C; i++) {
-            arr[i] = st.nextToken();
+            arr[i] = st.nextToken().charAt(0);
         }
 
-        Arrays.sort(arr);
+        Arrays.sort(arr); // 입력값에 대한 선정렬로 dfs 과정에서 정렬하는 과정 생략 가능
 
         dfs(0, 0);
     }
 
-    static void dfs(int start, int count) {
-        if (count == L) {
-            String word = "";
-            int a = 0;
-            int b = 0;
-            for (int i = 0; i < C; i++) {
-                if (visited[i]) {
-                    word += arr[i];
-                    if (arr[i].equals("a") || arr[i].equals("e") || arr[i].equals("i") || arr[i].equals("o") || arr[i].equals("u")) {
-                        a++;
-                    } else {
-                        b++;
-                    }
+    public static void dfs(int depth, int start) {
+        if (depth == L) {
+            if (isValid(code)) {
+                for (char c : code) {
+                    System.out.print(c);
                 }
+                System.out.println();
             }
-            if (a >= 1 && b >= 2) {
-                System.out.println(word);
-            }
+            return;
         }
-
         for (int i = start; i < C; i++) {
-            visited[i] = true;
-            dfs(i + 1, count + 1);
-            visited[i] = false;
+            code[depth] = arr[i];
+            dfs(depth + 1, i + 1);
         }
+    }
+
+    // 자음/모음 갯수 판별
+    public static boolean isValid(char[] code) {
+        int cons = 0;
+        int vow = 0;
+        for (char c : code) {
+            if (vlist.contains(c)) {
+                vow++;
+            } else {
+                cons++;
+            }
+        }
+        if (cons >= 2 && vow >= 1) return true;
+        else return false;
     }
 }
