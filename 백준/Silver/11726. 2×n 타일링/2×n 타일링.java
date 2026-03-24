d4031c34f59e96eafd4b53aa1a9ed5aae977a069
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-03-24 15:01:08 +0900

    [Silver III] Title: 2×n 타일링, Time: 112 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,22 +1,25 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
-    static int N;
+public class Main {
     static int[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        dp = new int[N + 1];
-        
-        dp[0] = 1;
+        int n = Integer.parseInt(br.readLine());
+
+        dp = new int[n + 3];
+
         dp[1] = 1;
-        for (int i = 2; i <= N; i++) {
-            if (i == 2) dp[2] = 2;
-            else dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        dp[2] = 2;
+        dp[3] = 3;
+
+        if (n > 3) {
+            for (int i = 4; i <= n; i++) {
+                dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+            }
         }
-        
-        System.out.println(dp[N]);
+
+        System.out.println(dp[n]);
     }
 }
