b187ed1820e016888e2befdeb8b02178844633e7
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-04-06 15:04:49 +0900

    [Silver IV] Title: 듣보잡, Time: 272 ms, Memory: 26284 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -1,42 +1,34 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
         Set<String> listen = new HashSet<>();
-        List<String> listLook = new ArrayList<>();
+        List<String> seeNotListen = new ArrayList<>();
 
         for (int i = 0; i < N; i++) {
-            listen.add(br.readLine().trim());
+            listen.add(br.readLine());
         }
 
         int count = 0;
-        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < M; i++) {
-            String input = br.readLine().trim();
-            if(listen.contains(input)) {
+            String inputSee = br.readLine();
+            if (listen.contains(inputSee)) {
                 count++;
-                listLook.add(input);
+                seeNotListen.add(inputSee);
             }
         }
 
-        Collections.sort(listLook);
-
+        seeNotListen.sort(Comparator.naturalOrder());
         System.out.println(count);
-        listLook.forEach(System.out::println);
+        for (String str : seeNotListen) {
+            System.out.println(str);
+        }
     }
 }
