1ae18f4340c7f91051eb60c0bf5e41c213cb6702
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-04-06 15:33:09 +0900

    [Silver IV] Title: 큐, Time: 164 ms, Memory: 19756 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,66 +1,41 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static String command;
-    static int commandInt;
-    
     public static void main(String[] args) throws IOException {
-        Queue<Integer> queue = new LinkedList<Integer>();
-        
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
         int N = Integer.parseInt(br.readLine());
-        
-        int i = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
+        int rear = 0;
 
-        String[] st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
 
-        while (i < N) {
-            st = br.readLine().trim().split(" ");
-            command = st[0];
-
-
-            if (st.length == 2) {
-                commandInt = Integer.parseInt(st[1]);
+            if (command.equals("push")) queue.add(rear = Integer.parseInt(st.nextToken()));
+            else if (command.equals("pop")) {
+                if (queue.isEmpty()) sb.append(-1).append("\n");
+                else sb.append(queue.poll()).append("\n");
             }
-
-            if (command.equals("push")) {
-                queue.add(commandInt);
-            } else if (command.equals("pop")) {
-                if (queue.size() == 0) {
-                    System.out.println(-1);
-                }
-                else {
-                    System.out.println(queue.poll());
-                }
-            } else if (command.equals("size")) {
-                System.out.println(queue.size());
-            } else if (command.equals("empty")) {
-                if (queue.size() == 0) {
-                    System.out.println(1);
-                } else {
-                    System.out.println(0);
-                }
-            } else if (command.equals("front")) {
-                if (queue.size() == 0) {
-                    System.out.println(-1);
-                }
-                else {
-                    System.out.println(queue.peek());
-                }
-            } else if (command.equals("back")) {
-                if (queue.size() == 0) {
-                    System.out.println(-1);
-                }
-                else {
-                    System.out.println(commandInt);
-                }
+            else if (command.equals("size")) sb.append(queue.size()).append("\n");
+            else if (command.equals("empty")) {
+                if (queue.isEmpty()) sb.append(1).append("\n");
+                else sb.append(0).append("\n");
             }
-            i++;
+            else if (command.equals("front")) {
+                if (queue.isEmpty()) sb.append(-1).append("\n");
+                else sb.append(queue.peek()).append("\n");
+            }
+            else if (command.equals("back")) {
+                if (queue.isEmpty()) sb.append(-1).append("\n");
+                else sb.append(rear).append("\n");
+            }
         }
+
+        System.out.print(sb);
     }
 }
