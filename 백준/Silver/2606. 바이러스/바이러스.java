8f731831c61e6901ecb48971580e732f0558dffe
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-04-07 07:39:23 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14060 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -2,61 +2,41 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
-    static boolean[][] graph;
-    static boolean[] visit;
-    static int count = 0;
+    static int C, T;
+    static int[][] graph;
+    static boolean[] visited;
+    static int result = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(br.readLine());
-        M = Integer.parseInt(br.readLine());
+        C = Integer.parseInt(br.readLine());
+        T = Integer.parseInt(br.readLine());
 
-        graph = new boolean[N + 1][N + 1];
-        visit = new boolean[N + 1];
+        graph = new int[C + 1][C + 1];
+        visited = new boolean[C + 1];
 
         StringTokenizer st;
-        for (int i = 0; i < M; i++) {
+
+        for (int i = 0; i < T; i++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            graph[x][y] = graph[y][x] = true;
+            graph[x][y] = graph[y][x] = 1;
         }
 
-        bfs(1);
+        dfs(1);
 
-        System.out.println(count);
-
+        System.out.println(result);
     }
 
-    public static void dfs(int v) {
-        visit[v] = true;
-
-        if (v == graph.length) {
-            return;
-        }
-        for (int i = 1; i < graph.length; i++) {
-            if (graph[v][i] && !visit[i]) {
+    static void dfs(int x){
+        visited[x] = true;
+        
+        for(int i = 1; i <= C; i++){
+            if(graph[x][i] == 1 && !visited[i]){
                 dfs(i);
-                count++;
-            }
-        }
-    }
-
-    public static void bfs(int v) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(v);
-        visit[v] = true;
-
-        while (!queue.isEmpty()) {
-            int tmp = queue.poll();
-            for (int i = 1; i < graph.length; i++) {
-                if (graph[tmp][i] && !visit[i]) {
-                    queue.add(i);
-                    visit[i] = true;
-                    count++;
-                }
+                result++;
             }
         }
     }
