c1a83094ddff787196e2d6306edaca6256583fd0
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-04-06 17:37:39 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,6 +1,5 @@
--- 코드를 입력하세요
-SELECT b.title, b.board_id, r.reply_id, r.writer_id, r.contents, DATE_FORMAT(r.created_date, "%Y-%m-%d")
-FROM used_goods_board b
-JOIN used_goods_reply r ON b.board_id = r.board_id
-WHERE DATE_FORMAT(b.created_date, "%Y-%m") = "2022-10"
-ORDER BY r.created_date asc, b.title asc;
+SELECT b.TITLE, b.BOARD_ID, r.REPLY_ID, r.WRITER_ID, r.CONTENTS, DATE_FORMAT(r.CREATED_DATE, '%Y-%m-%d') as CREATED_DATE
+FROM USED_GOODS_BOARD as b, USED_GOODS_REPLY as r
+WHERE b.BOARD_ID = r.BOARD_ID 
+    AND DATE_FORMAT(b.CREATED_DATE, '%Y-%m') = '2022-10' 
+ORDER BY CREATED_DATE ASC, b.TITLE ASC;
