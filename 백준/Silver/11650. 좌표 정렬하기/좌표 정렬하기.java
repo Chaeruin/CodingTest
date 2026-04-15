f46b8a0e8346f682ccf529160fd570acdf7dfd2d
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-04-15 12:07:07 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 588 ms, Memory: 48536 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,34 +1,39 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(br.readLine());
+        List<int[]> list = new ArrayList<>();
 
-        int[][] list = new int[N][2];
+        StringTokenizer st;
 
         for (int i = 0; i < N; i++) {
-            String[] input = br.readLine().split(" ");
-            list[i][0] = Integer.parseInt(input[0]);
-            list[i][1] = Integer.parseInt(input[1]);
+            st = new StringTokenizer(br.readLine());
+            int first = Integer.parseInt(st.nextToken());
+            int second = Integer.parseInt(st.nextToken());
+            int[] arr = {first, second};
+            list.add(arr);
         }
 
-        Arrays.sort(list, (x, y) -> {
-            if (x[0] == y[0]) {
-                return x[1] - y[1];
+        list.sort(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] != o2[0]) {
+                    return o1[0] - o2[0];
+                }
+                return o1[1] - o2[1];
             }
-            return x[0] - y[0];
         });
 
-        for (int i = 0; i < N; i++) {
-            System.out.println(list[i][0] + " " + list[i][1] );
+        StringBuilder sb = new StringBuilder();
+
+        for (int[] arr : list) {
+            sb.append(arr[0]).append(" ").append(arr[1]).append("\n");
         }
+
+        System.out.println(sb);
     }
 }
