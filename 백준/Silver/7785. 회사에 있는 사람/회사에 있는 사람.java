52e67b00cd3627b53b8ead8bd1c48aa1d0abe772
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-04-15 13:15:32 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 592 ms, Memory: 48852 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -1,35 +1,37 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        Map<String, String> enterLog = new HashMap<>();
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+
+        Set<String> set = new HashSet<>();
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             String name = st.nextToken();
-            String entLeave = st.nextToken();
-            if (enterLog.containsKey(name) && entLeave.equals("leave")) {
-                enterLog.remove(name);
-            } else {
-                enterLog.put(name, entLeave);
+            String command = st.nextToken();
+            if (command.equals("enter")) {
+                set.add(name);
+            } else if (command.equals("leave")) {
+                set.remove(name);
             }
         }
+        
+        List<String> list = new ArrayList<>(set);
 
-        List<String> nameLog = new ArrayList<>(enterLog.keySet());
-        nameLog.sort(Collections.reverseOrder());
+        list.sort(Comparator.reverseOrder());
 
-        nameLog.forEach(System.out::println);
+        StringBuilder sb = new StringBuilder();
+
+        for (String name : list) {
+            sb.append(name).append("\n");
+        }
+
+        System.out.println(sb);
     }
 }
