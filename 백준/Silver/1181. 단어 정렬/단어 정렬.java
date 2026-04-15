e7c46323d91d24dd43cea250d4934a9da23ac7ae
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-04-15 11:56:19 +0900

    [Silver V] Title: 단어 정렬, Time: 336 ms, Memory: 24888 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -4,26 +4,34 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int N = Integer.parseInt(br.readLine());
 
         Set<String> set = new HashSet<>();
 
         for (int i = 0; i < N; i++) {
-            set.add(br.readLine());
+            String input = br.readLine();
+            set.add(input);
         }
 
         List<String> list = new ArrayList<>(set);
 
-        list.sort( new Comparator<String>() {
+        list.sort(new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
-                if (o1.length() != o2.length()) return o1.length() - o2.length();
-                else return o1.compareTo(o2);
+                if (o1.length() != o2.length()) {
+                    return o1.length() - o2.length();
+                }
+                return o1.compareTo(o2);
             }
         });
 
+        StringBuilder sb = new StringBuilder();
+
         for (String str : list) {
-            System.out.println(str);
+            sb.append(str).append("\n");
         }
+
+        System.out.println(sb);
     }
 }
