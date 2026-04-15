d11a167bd47f5e58ccfd63d9de079e2d5225ab80
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-04-15 12:29:39 +0900

    [Silver V] Title: 소트인사이드, Time: 108 ms, Memory: 14472 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,31 +1,23 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String N = br.readLine();
-        int n = Integer.parseInt(N);
+        String[] input = br.readLine().split("");
 
-        List<Integer> list = new ArrayList<>();
+        Integer[] number = Arrays.stream(input)
+                .map(Integer::valueOf)
+                .toArray(Integer[]::new);
 
-        for (int i = N.length() - 1; i >= 0; i--) {
-            int a = (int) (n / Math.pow(10, i));
-            list.add(a);
-            n -= (int) (a * Math.pow(10, i));
+        Arrays.sort(number, Collections.reverseOrder());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int n : number) {
+            sb.append(n);
         }
 
-        list.sort(Collections.reverseOrder());
-
-        String result = list.stream()
-                        .map(String::valueOf)
-                        .collect(Collectors.joining(""));
-
-        System.out.println(result);
+        System.out.println(sb);
     }
 }
