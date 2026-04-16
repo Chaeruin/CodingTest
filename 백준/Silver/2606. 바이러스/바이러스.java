a47f8a9ec701a2483e004ec4cc8e2e24865205dd
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-04-16 15:37:21 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,42 +1,42 @@
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int C, T;
-    static int[][] graph;
+
+    static boolean[][] graph;
     static boolean[] visited;
-    static int result = 0;
+    static int C, L, count = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         C = Integer.parseInt(br.readLine());
-        T = Integer.parseInt(br.readLine());
+        L = Integer.parseInt(br.readLine());
 
-        graph = new int[C + 1][C + 1];
+        graph = new boolean[C + 1][C + 1];
         visited = new boolean[C + 1];
 
         StringTokenizer st;
 
-        for (int i = 0; i < T; i++) {
+        for (int i = 0; i < L; i++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            graph[x][y] = graph[y][x] = 1;
+            graph[x][y] = graph[y][x] = true;
         }
 
         dfs(1);
-
-        System.out.println(result);
+        System.out.println(count);
     }
 
-    static void dfs(int x){
+    static void dfs(int x) {
         visited[x] = true;
-        
+
         for(int i = 1; i <= C; i++){
-            if(graph[x][i] == 1 && !visited[i]){
+            if(graph[x][i] && !visited[i]){
                 dfs(i);
-                result++;
+                count++;
             }
         }
     }
