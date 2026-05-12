80ca78dfdecf20115fa02f401efe165d773a8214
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-05-12 11:49:34 +0900

    [Passed] 2개씩 그룹짓기 (103ms, 10MB)

---
+++ 260512/2개씩 그룹짓기/group-of-pairs.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[2 * N];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < 2 * N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < N; i++) {
+            int sum = arr[i] + arr[2 * N - 1 - i];
+            max = Math.max(max, sum);
+        }
+
+        System.out.println(max);
+
+    }
+}
