020855c89a7d135a4ba3f065d087bc9adcdae801
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-06-12 08:03:49 +0900

    [Passed] 두 점으로 만드는 직사각형 (91ms, 9MB)

---
+++ 260612/두 점으로 만드는 직사각형/rectangle-made-of-two-points.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x1 = Integer.parseInt(st.nextToken());
+        int y1 = Integer.parseInt(st.nextToken());
+        int x2 = Integer.parseInt(st.nextToken());
+        int y2 = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        int a1 = Integer.parseInt(st.nextToken());
+        int b1 = Integer.parseInt(st.nextToken());
+        int a2 = Integer.parseInt(st.nextToken());
+        int b2 = Integer.parseInt(st.nextToken());
+
+        int minX1 = Math.min(x1, a1);
+        int minY1 = Math.min(y1, b1);
+        int maxX1 = Math.max(x2, a2);
+        int maxY1 = Math.max(y2, b2);
+
+        System.out.println((maxX1 - minX1) * (maxY1 - minY1));
+    }
+}
