0937f20f232a4c2484c7c9d2b535fef861d872e9
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-06-24 13:24:39 +0900

    [Passed] 수열 조작 (143ms, 12MB)

---
+++ 260624/수열 조작/sequence-manipulation.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Deque<Integer> dq = new ArrayDeque<>();
+
+        for (int i = 1; i <= N; i++) {
+            dq.addLast(i);
+        }
+
+        while (!(dq.size() == 1)) {
+            dq.pollFirst();
+            dq.addLast(dq.pollFirst());
+        }
+
+        System.out.println(dq.pollFirst());
+    }
+}
