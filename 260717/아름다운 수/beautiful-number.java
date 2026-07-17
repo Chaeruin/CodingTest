f58c451ff94b32389ab6260594c7426a9dab8a27
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-07-17 11:00:46 +0900

    [Passed] 아름다운 수 (90ms, 9MB)

---
+++ 260717/아름다운 수/beautiful-number.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static int N, answer = 0;
+    public static int[] selected;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        selected = new int[N];
+
+        dfs(0);
+
+        System.out.println(answer);
+    }
+
+    public static void dfs(int len) {
+
+        if (len == N) {
+            answer++;
+            return;
+        }
+
+        if (len > N) {
+            return;
+        }
+
+        // 1
+        dfs(len + 1);
+
+        // 22
+        dfs(len + 2);
+
+        // 333
+        dfs(len + 3);
+
+        // 4444
+        dfs(len + 4);
+    }
+
+}
+ 
