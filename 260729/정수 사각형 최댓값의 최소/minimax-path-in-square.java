dbe698d8da6612a8b62e7ee8b1aaecb87985df8e
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-07-29 13:00:09 +0900

    [Passed] 정수 사각형 최댓값의 최소 (158ms, 11MB)

---
+++ 260729/정수 사각형 최댓값의 최소/minimax-path-in-square.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static int N;
+    public static int[][] grid;
+    public static int[][] dp;
+    // 우 , 하
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        grid = new int[N][N];
+        dp = new int[N][N];
+
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                grid[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dp[0][0] = grid[0][0];
+
+        for (int i = 1; i < N; i++) {
+            dp[0][i] = Math.max(dp[0][i - 1], grid[0][i]);
+        }
+
+        for (int i = 1; i < N; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], grid[i][0]);
+        }
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 1; j < N; j++) {
+                dp[i][j] = Math.max(Math.min(dp[i][j - 1], dp[i - 1][j]), grid[i][j]);
+            }
+        }
+
+        System.out.println(dp[N - 1][N - 1]);
+    }
+}
