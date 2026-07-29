61e4058ab3cfc80892b274b54550416ac1290e97
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-07-29 10:22:29 +0900

    [Passed] 정수 사각형 최소 합 (140ms, 11MB)

---
+++ 260729/정수 사각형 최소 합/minimum-sum-path-in-square.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static int N;
+    public static int[][] grid;
+    public static int[][] dp;
+
+    // public static int[] dx = {0, 1};
+    // public static int[] dy = {-1, 0};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        grid = new int[N][N];
+        dp = new int[N][N];
+
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                grid[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dp[0][N - 1] = grid[0][N - 1];
+
+        for (int i = 1; i < N; i++) {
+            dp[i][N - 1] = dp[i - 1][N - 1] + grid[i][N - 1];
+        }
+        for (int i = N - 2; i >= 0; i--) {
+            dp[0][i] = dp[0][i + 1] + grid[0][i];
+        }
+
+
+        for (int i = 1; i < N; i++) {
+            for (int j = N - 2; j >= 0; j--) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j + 1]) + grid[i][j];
+            }
+        }
+
+        System.out.println(dp[N - 1][0]);
+    }
+}
