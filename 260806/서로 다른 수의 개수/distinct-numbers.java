6899722d0de58afae1ed304f3dcf687fc06aec2b
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-08-06 08:39:56 +0900

    [Passed] 서로 다른 수의 개수 (298ms, 22MB)

---
+++ 260806/서로 다른 수의 개수/distinct-numbers.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        HashSet<Integer> set = new HashSet<>();
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+
+        System.out.println(set.size());
+    }
+}
+ 
