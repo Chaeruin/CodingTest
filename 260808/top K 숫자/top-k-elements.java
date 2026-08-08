efc991780c549a57ce45fa1fff3937e7a672a278
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-08-08 10:35:42 +0900

    [Passed] top K 숫자 (1068ms, 35MB)

---
+++ 260808/top K 숫자/top-k-elements.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        TreeSet<Integer> set = new TreeSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(sc.nextInt());
+        }
+        
+        for (int i = 0; i < k; i++) {
+            int num = set.last();
+            System.out.print(num + " ");
+            set.remove(num);
+        }
+    }
+}
