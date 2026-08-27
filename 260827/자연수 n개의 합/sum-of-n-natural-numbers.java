0de88a63399fa316d1d9ad5e07ec271309c8c271
Chaeruin <51053653+Chaeruin@users.noreply.github.com>
2026-08-27 10:58:26 +0900

    [Passed] 자연수 n개의 합 (127ms, 10MB)

---
+++ 260827/자연수 n개의 합/sum-of-n-natural-numbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long s = sc.nextLong();
+        
+        long left = 1;                         
+        long right = 2_000_000_000;                     
+        long maxNum = 0;                     
+        while (left <= right) {            
+            long mid = (left + right) / 2;      
+            if(mid * (mid + 1) / 2 <= s) {   
+                left = mid + 1;              
+                maxNum = Math.max(maxNum, mid);
+            }
+            else                               
+                right = mid - 1;               
+        }
+
+        System.out.print(maxNum);             
+    }
+}
